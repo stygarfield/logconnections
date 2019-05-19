230d35f885920481e900ad9b4cebd83a45871f9b
david63 <david.wood@pear-tree.org.uk>
2019-05-19 11:00:58 +0100

    fix

--- controller/admin_controller.php
+++ controller/admin_controller.php
@@ -9,13 +9,13 @@
 
 namespace david63\logconnections\controller;
 
-use phpbb\config\config;
+use phpbb\request\request;
 use phpbb\template\template;
 use phpbb\user;
 use phpbb\log\log;
 use phpbb\language\language;
-use phpbb\request\request;
 use david63\logconnections\ext;
+use phpbb\config\config;
 
 /**
 * Admin controller
